2dcec5ed558c9cce8bbf4a2a743307e7cfade64b
Juan Carlos Garcia Bastidas <juan.c.garcia.b@correounivalle.edu.co>
2016-04-22 18:57:21 -0500

    add matirialize

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,7 @@
+@extends('templates.dashboard')
+
+@section('content')
+
+<h1>Holi</h1>
+
+@endsection
---
+++ resources/views/templates/dashboard.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="{{ asset('../resources/assets/materialize/css/materialize.min.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
