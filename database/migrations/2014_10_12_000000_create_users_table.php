4e995e5f39c1a56ed6fd062ed4a84086e91c0222
Juan Carlos Garcia Bastidas <juan.c.garcia.b@correounivalle.edu.co>
2016-04-22 18:22:23 -0500

    User migration created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->string('dependencia');
+            $table->enum('tipo', ['administrador', 'usuario']);
             $table->timestamps();
+
         });
     }
 
